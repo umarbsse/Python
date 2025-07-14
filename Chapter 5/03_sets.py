379d7abdcfafb635e7ca9877ba37b4b82b94e07c
umarbsse <umarbsse@gmail.com>
2025-07-14 23:14:15 +0500

    chapter 5

---
+++ Chapter 5/03_sets.py
@@ -0,0 +1,15 @@
+#set and dictionary both use same curly brackets
+
+s = {}  # like the empty Dictionary
+print(type(s))
+
+
+empty_set = set() # It creates an empty set
+
+print(type(empty_set))
+
+set_1 = {1,2,3,4,5,6,67,7,8,8,1,1,1,1,1,1}   # set does not get reptative elements , Its auto rejects the sets
+
+# order does not maintain in sets, if want to maintain order get use list instead of set
+
+print(set_1)
---
+++ Chapter 5/03_sets_methods.py
@@ -0,0 +1,26 @@
+set_1 = {1,2,3}
+
+set_1.add(67) # add elements in set
+print(set_1)
+
+
+print(len(set_1)) # get length of set
+
+set_1.remove(2)  # remove the elemnt from set
+
+set_1.pop() # remove the random elemnt from set
+
+#set_1.clear()    #empty the set
+
+
+print(set_1)
+
+
+
+set_2 = {12,123,456,456,1,2,3}
+
+
+print(set_1.union(set_2))
+
+
+print(set_1.intersection(set_2))
