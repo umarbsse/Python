9f87e3df311c61da59107cd61b7ba2f041bf1cc5
umarbsse <umarbsse@gmail.com>
2025-07-22 23:22:39 +0500

    func

---
+++ Chapter 8/04_with_prams_return_value.py
@@ -0,0 +1,12 @@
+def good_day(name, ending):
+    print("Good Morning, "+name)
+    print(ending)
+    return "done"
+
+
+a = good_day("Essa", "Thanks")
+print(a)
+good_day("Ami", "Thank You")
+print(a)
+good_day("Khurram", "Thank You")
+print(a)
---
+++ Chapter 8/05_with_prams_default_value.py
@@ -0,0 +1,7 @@
+def good_day(name, ending="Thanks"):
+    print("Good Morning, "+name)
+    print(ending)
+
+good_day("Essa")
+
+good_day("Khurram", "Thank You")
